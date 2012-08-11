5739d6db623d4a5e86a9f0044339fd5826a247f4
Sean Robertson <sprobertson@gmail.com>
2012-08-11 07:38:19 +0000

    trying to fix encodings

--- templating.py
+++ templating.py
@@ -27,8 +27,9 @@ class ReloadingLoader(BaseLoader):
 def timestamp_to_nicedate(timestamp):
     return datetime.datetime.fromtimestamp(timestamp).strftime("%b %d, %Y @ %I:%M %p")
 def remove_html_tags(data):
+    if not data: return ''
     p = re.compile(r'<.*?>')
-    return p.sub('', data)
+    return p.sub('', data.encode('latin1').decode('utf8','ignore'))
 jinja2_filters = {
     'to_nicedate': timestamp_to_nicedate,
     'sanitize': remove_html_tags
