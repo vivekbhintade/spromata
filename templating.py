b0cd020171bf4e6b653adbcf321bddf39a5de9e5
Sean Robertson <sprobertson@gmail.com>
2012-08-10 22:40:36 -0700

    better stripper

--- templating.py
+++ templating.py
@@ -25,19 +25,12 @@ class ReloadingLoader(BaseLoader):
 
 def timestamp_to_nicedate(timestamp):
     return datetime.datetime.fromtimestamp(timestamp).strftime("%b %d, %Y @ %I:%M %p")
-from HTMLParser import HTMLParser
-class MLStripper(HTMLParser):
-    def __init__(self):
-        self.reset()
-        self.fed = []
-    def handle_data(self, d):
-        self.fed.append(d)
-    def get_data(self):
-        return ''.join(self.fed)
-def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data().replace('\\', '')
+def removeTags(html):
+    soup = BeautifulSoup(html)
+    for tag in ['a','script','link','div','form']:
+        for tag in soup.findAll(tag):
+            tag.replaceWith("")
+    return str(soup)
 jinja2_filters = {
     'to_nicedate': timestamp_to_nicedate,
     'sanitize': strip_tags
