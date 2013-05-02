c67d9cef738d0182dd7a0d0789b2bb1487e8fe23
Sean Robertson <sprobertson@gmail.com>
2013-05-02 16:27:54 -0400

    slightly better wordify

--- make_keywords.py
+++ make_keywords.py
@@ -2,25 +2,27 @@ import re
 import random
 import Stemmer
 from functional import *
-import re, htmlentitydefs
+import htmlentitydefs
+import nltk
 
 stemmer = Stemmer.Stemmer('english')
 
-STOP_WORDS = (
+STOP_WORDS = set(nltk.corpus.stopwords.words('english') + (
     "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,"
     "be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,"
     "ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,"
-    "i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,"
+    "i,i'm,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,"
     "my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,"
     "say,says,she,should,since,so,some,than,that,the,their,them,then,there,"
     "these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,"
     "which,while,who,whom,why,will,with,would,yet,you,your,"
     "http,www,com,"
-    "etsy,shop,product,item"
-).split(',')
+    "wordpress,"
+    "don't,know,think,want,more"
+).split(','))
 
 wordify = lambda s: re.findall('[a-z]+', s.lower())
-goodwords = lambda ws: [w for w in ws if w not in STOP_WORDS and len(w) > 2]
+goodwords = lambda ws: [w for w in ws if w not in STOP_WORDS and len(w) > 2 and len(w) < 20]
 
 def get_keywords(*word_sources):
     return goodwords(stemmer.stemWords(set( wordify(' '.join(word_sources)) )))
