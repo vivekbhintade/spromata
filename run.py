12a8bd1ab05f57a529998e914915bf71042905f3
Sean Robertson <sprobertson@gmail.com>
2013-05-02 16:34:57 -0400

    useless reloader config

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import config
 if config.server_type == 'gevent': import gevent.monkey; gevent.monkey.patch_all()
 from server import *
 bottle.debug=config.debug
-bottle.run(host=config.server_host, port=config.server_port, server=config.server_type, reloader=config.reloader)
+bottle.run(host=config.server_host, port=config.server_port, server=config.server_type)
